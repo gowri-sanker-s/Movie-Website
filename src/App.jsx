6aabc3c53808c0124d1504ccc1f8424e6291ba25
Gowri Sanker <amyv461@gmail.com>
2023-12-18 23:32:53 +0530

    Commit after 1st change

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+
+function App() {
+  const [searchMovie, setSearchMovie] = useState("");
+  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+  const handleSearchMovie = (e) => {
+    console.log("functionCalled");
+    e.preventDefault();
+    axios
+      .get(
+        `http://www.omdbapi.com/?i=tt3896198&apikey=f8e77284&s=${searchMovie}`
+      )
+      .then((response) => {
+        setMovies(response.data.Search);
+        console.log(response.data.Search);
+      });
+  };
+  const movieDetails = (imdbID) => {
+    navigate(`/movieDetails/${imdbID}`);
+    console.log(imdbID);
+  };
+  return (
+    <div className="App">
+      <h1 className="mainHeading">CMOVIE</h1>
+      <form
+        onSubmit={handleSearchMovie}
+        className={`form ${movies.length > 0 ? "formMovie" : "formNoMovie"}`}
+      >
+        <input
+          type="text"
+          placeholder="Search Movies"
+          value={searchMovie}
+          onChange={(e) => setSearchMovie(e.target.value)}
+        />
+        <button>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+        </button>
+      </form>
+      <h3 className={`${!movies.length > 0 ? 'showDiscovery' : 'hideDiscovery'}`}>
+        Discover and Explore: Your Cinematic Journey Begins Here!
+      </h3>
+      {!movies.length > 0 ? (
+        <div className="noMovies"></div>
+      ) : (
+        <div className="moviePosters">
+          {movies.map((movie, index) => {
+            return (
+              <div
+                className="container"
+                key={movie.imdbID}
+                onClick={() => movieDetails(movie.imdbID)}
+              >
+                <img
+                  src={
+                    movie.Poster !== "N/A"
+                      ? movie.Poster
+                      : "https://via.placeholder.com/400"
+                  }
+                  alt={movie.Title}
+                  className="movieImage"
+                />
+                <p className="movieTitle">
+                  {movie.Title} - {movie.Year}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/MovieDetails.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import '../css/movieDetails.css'
+const MovieDetails = () => {
+  const imdbID = useParams();
+  console.log(imdbID.id);
+  const [movieDetails, setMovieDetails] = useState({});
+  useEffect(() => { 
+    axios
+      .get(`http://www.omdbapi.com/?i=${imdbID.id}&apikey=f8e77284&plot=full`)
+      .then((response) => {
+        setMovieDetails(response.data);
+        console.log(response.data);
+      });
+  }, [imdbID.id]);
+  return (
+    <div>
+{/*       <h1 className="mainHead">MovieDetails</h1> */}
+      <h2 className="title">{movieDetails.Title}</h2>
+      <div className="imgContainer">
+      <img
+        src={
+          movieDetails.Poster !== "N/A"
+            ? movieDetails.Poster
+            : "https://via.placeholder.com/400"
+        }
+        alt={movieDetails.Title}
+        className="detailsPoster"
+      />
+      </div>
+      <div className="subtypes">
+      <p className="type">{movieDetails.Type}</p>
+      <p className="rated">{movieDetails.Rated}</p>
+      </div>
+      <div className="main-container">
+
+
+      <p className="plot">{movieDetails.Plot}</p>
+      <p className="released">Release Date : {movieDetails.Released}</p>
+      <p className="runtime">Runtime : {movieDetails.Runtime}</p>
+      <p className="language">Languages : {movieDetails.Language}</p>
+      <p className="director">Director : {movieDetails.Director}</p>
+      <p className="actors">Cast : {movieDetails.Actors}</p>
+
+      <div className="ratingSitesImg">
+        <img src="/Assets/imdb.png" alt="no" />
+        <img src="/Assets/rt.jpg" alt="no" />
+        <img src="/Assets/metacritic.png" alt="no" />
+      </div>
+      {movieDetails.Ratings && movieDetails.Ratings.length > 0 ? (
+        <div className="outer-ratingSites">{movieDetails.Ratings.map((rating,index) => {
+            return(
+                <div key={index} className="ratingSites">
+
+                    <p className="value">{rating.Value}</p>
+                </div>
+            )
+        })}</div>
+      ) : (
+        <div>None</div>
+      )}
+
+
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetails
